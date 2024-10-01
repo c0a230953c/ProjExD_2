30056445455b591912d2df5935143338415c88b2
sou <c0a230953c@edu.teu.ac.jp>
2024-10-01 16:38:03 +0900

    演習１ ゲームオーバー画面

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,29 +1,33 @@
 import os
 import random
 import sys
+import time
 import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
-    pg.K_UP:(0,-5),
-    pg.K_DOWN:(0,+5),
-    pg.K_LEFT:(-5,0),
-    pg.K_RIGHT:(+5,0),
+    pg.K_UP:(0, -5),
+    pg.K_DOWN:(0, +5),
+    pg.K_LEFT:(-5, 0),
+    pg.K_RIGHT:(+5, 0),
     }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-def check_bound(obj_rct:pg.Rect) -> tuple[bool,bool]:
+
+
+def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     """
-    引数:こうかとん または 爆弾のRect
+    引数：こうかとん　または　爆弾のRect
     戻り値：真理値タプル（横判定結果、縦判定結果）
-    画面内ならTure、画面外ならFalse
+    画面内ならTrue、画面外ならFalse
     """
-    yoko,tate = True,True
+    yoko, tate = True, True
     if obj_rct.left < 0 or WIDTH < obj_rct.right:
         yoko = False
     if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
         tate = False
-    return yoko,tate
+    return yoko, tate    
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -32,13 +36,19 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    bd_img = pg.Surface((20,20))
-    bd_img.set_colorkey((0,0,0))
-    pg.draw.circle(bd_img,(255,0,0),(10,10),10)
+    bd_img = pg.Surface((20, 20))
+    bd_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bd_img, (255, 0, 0), (10, 10), 10)
     bd_rct = bd_img.get_rect()
-    bd_rct.centerx = random.randint(0,WIDTH)
-    bd_rct.centery = random.randint(0,HEIGHT)
-    vx,vy = +5,-5
+    bd_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, -5
+    gob_img = pg.Surface((1100, 650))
+    gob_img.set_alpha(128)
+    pg.draw.rect(gob_img, (0, 0, 0), pg.Rect(0, 0, 800, 1600))
+    fonto = pg.font.Font(None, 80)
+    txt = fonto.render("GameOver", True, (255, 0, 255))
+    cry_kk_img = pg.image.load("fig\8.png")
+    
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -46,30 +56,37 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
-        if kk_rct.colliderect(bd_rct): 
-            print("ゲームオーバー") # 工科トンと爆弾が重なっていたら
+        if kk_rct.colliderect(bd_rct): #工科トンと爆弾が重なっていたら
+            screen.blit(gob_img, (0, 0))
+            screen.blit(txt, [420, 280])
+            screen.blit(cry_kk_img, (360, 280))
+            screen.blit(cry_kk_img, (730, 280))
+            pg.display.flip()
+            time.sleep(5)
             return
+
         key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]#横座標、縦座標
-        #if key_lst[pg.K_UP]:
-            #sum_mv[1] -= 5
-        #if key_lst[pg.K_DOWN]:
-            #sum_mv[1] += 5
-        #if key_lst[pg.K_LEFT]:
-            #sum_mv[0] -= 5
-        #if key_lst[pg.K_RIGHT]:
-            #sum_mv[0] += 5
-        for key,tpl  in DELTA.items():
+        sum_mv = [0, 0] #横座標, 縦座標
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
+
+        for key, tpl in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += tpl[0]  # 横方向
-                sum_mv[1] += tpl[1]  # 縦座標
+                sum_mv[0] += tpl[0] #横方向
+                sum_mv[1] += tpl[1] #縦方向
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True,True):
-            kk_rct.move_ip(-sum_mv[0],-sum_mv[1])
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
 
-        bd_rct.move_ip((vx,vy))
-        yoko,tate = check_bound(bd_rct)
+        bd_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bd_rct)
         if not yoko:
             vx *= -1
         if not tate:
